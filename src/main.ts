ea32c68abada8cc538470c39e4ccfd4a3289f2d1
Billy Cao <the.billy.c@gmail.com>
2024-01-15 01:53:09 -0800

    Add pino logger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 
 // import config from 'config';
+import { pino } from 'pino';
+
+const logger = pino();
 
 if (import.meta.url.endsWith(process.argv[1])) {
+  logger.info('hello');
   console.log('hello');
 }
